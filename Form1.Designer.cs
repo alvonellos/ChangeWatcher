90ddf667fa4d279598827d0a2632e8839ef2fe8e
Alexander Alvonellos <alexander.alvonellos@unix.net>
2012-11-05 16:48:49 -0500

    Initial Commit
    Initial Commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ChangeWatcher {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(991, 513);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.Location = new System.Drawing.Point(12, 531);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(991, 26);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Start";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 569);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Change Watcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ChangeWatcher {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        public FileSystemWatcher watcher;
+        public delegate void ArgumentChanged(string text);
+        public delegate void ArgumentCreated(string text);
+        public delegate void ArgumentDeleted(string text);
+        public delegate void ArgumentRenamed(string text);
+        public ArgumentChanged Changed;
+        public ArgumentCreated Created;
+        public ArgumentDeleted Deleted;
+        public ArgumentRenamed Renamed;
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            if (this.checkBox1.Text == "Start") {
+                subscribe();
+                this.checkBox1.Text = "Stop";
+            } else {
+                unsubscribe();
+                this.checkBox1.Text = "Start";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            watcher = new FileSystemWatcher();
+            watcher.Path = System.IO.Path.GetPathRoot(System.Environment.CurrentDirectory);
+            watcher.Filter = "*";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = (NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.Security | NotifyFilters.Size);
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.Deleted += new FileSystemEventHandler(OnDeleted);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+            Changed = new ArgumentChanged(Ch);
+            Created = new ArgumentCreated(Cr);
+            Deleted = new ArgumentDeleted(Dl);
+            Renamed = new ArgumentRenamed(Rn);
+        }
+
+        public void OnChanged(object sender, FileSystemEventArgs e) {
+            string data = DateTime.Now + " | " + "Changed" + " | " + "File: " + " | " + e.Name + "\n";
+            this.Invoke(Changed, data);
+        }
+        public void OnCreated(object sender, FileSystemEventArgs e) {
+            string data = DateTime.Now + " | " + "Created" + " | " + "File: " + " | " + e.Name + "\n";
+            this.Invoke(Created, data);
+        }
+        public void OnDeleted(object sender, FileSystemEventArgs e) {
+            string data = DateTime.Now + " | " + "Deleted" + " | " + "File: " + " | " + e.Name + "\n";
+            this.Invoke(Deleted, data);
+        }
+        public void OnRenamed(object sender, RenamedEventArgs e) {
+            string data = DateTime.Now + " | " + "Renamed" + " | " + "File: " + " | " + e.Name + "\n";
+            this.Invoke(Renamed, data);
+        }
+
+        private void Ch(string data) { this.textBox1.AppendText(data); }
+        private void Cr(string data) { this.textBox1.AppendText(data); }
+        private void Dl(string data) { this.textBox1.AppendText(data); }
+        private void Rn(string data) { this.textBox1.AppendText(data); }
+
+        private void subscribe() {
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void unsubscribe() {
+            watcher.EnableRaisingEvents = false;
+        }
+    }
+}
